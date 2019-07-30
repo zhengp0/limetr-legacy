fix: fix the delta in the trimming objective and gradient

limetr/__init__.py
@@ -401,9 +401,9 @@ class LimeTr:
         if self.std_flag == 0:
             v = self.V
         elif self.std_flag == 1:
-            v = np.repeat(delta[0], self.N)
+            v = np.repeat(self.delta[0], self.N)
         elif self.std_flag == 2:
-            v = np.repeat(delta, self.n)
+            v = np.repeat(self.delta, self.n)
         d = v + t
 
         val = 0.5*np.sum(r**2*w/d)
@@ -428,9 +428,9 @@ class LimeTr:
         if self.std_flag == 0:
             v = self.V
         elif self.std_flag == 1:
-            v = np.repeat(delta[0], self.N)
+            v = np.repeat(self.delta[0], self.N)
         elif self.std_flag == 2:
-            v = np.repeat(delta, self.n)
+            v = np.repeat(self.delta, self.n)
         d = v + t
 
         g = 0.5*r/d