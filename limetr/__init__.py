fix: fix the variance in the trimming objective and gradient.

limetr/__init__.py
@@ -398,7 +398,12 @@ class LimeTr:
     def objectiveTrimming(self, w):
         t = (self.Z**2).dot(self.gamma)
         r = self.Y - self.X(self.beta)
-        v = self.V
+        if self.std_flag == 0:
+            v = self.V
+        elif self.std_flag == 1:
+            v = np.repeat(delta[0], self.N)
+        elif self.std_flag == 2:
+            v = np.repeat(delta, self.n)
         d = v + t
 
         val = 0.5*np.sum(r**2*w/d)
@@ -420,7 +425,12 @@ class LimeTr:
 
         t = (self.Z**2).dot(self.gamma)
         r = (self.Y - self.X(self.beta))**2
-        v = self.V
+        if self.std_flag == 0:
+            v = self.V
+        elif self.std_flag == 1:
+            v = np.repeat(delta[0], self.N)
+        elif self.std_flag == 2:
+            v = np.repeat(delta, self.n)
         d = v + t
 
         g = 0.5*r/d